e8a2529ed26346714c1065fe550f3f946455d59d
Dzaky-Hadyan <dzakyhadyaneliyanta06@gmail.com>
2024-12-10 14:14:54 +0700

    idk

--- api/index.php
+++ api/index.php
@@ -21,8 +21,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perkenalan</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="./style.css">
+    <script src="./script.js"></script>
 </head>
 <body>
     <nav>
